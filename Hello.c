95227000571e6e1f12d36faed9be183faf20654f
masudrana1221 <masud.mr5622@gmail.com>
2024-10-07 16:52:50 +0800

    add my new code

--- Hello.c
+++ Hello.c
@@ -1,4 +1,18 @@
-#include<stdio.h>
-int main(){
-   printf("I love Programming");
+#include <stdio.h>
+int main()
+{
+   int M;
+   scanf("%d", &M);
+   for (int i = 1; i <= M; i++)
+   {
+      if (i % 3 == 0 || i % 5 == 0)
+      {
+         printf("%d Yes\n", i);
+      }
+      else
+      {
+         printf("%d No\n", i);
+      }
+   }
+   return 0;
 }
---
+++ first.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+int main(){
+   // A student name is Masud Rana and you have to store his data on varsity website.
+
+   // Variables
+   char Name = 'M';
+   int hisRoll = 202938;
+   int hisAge = 23;
+   float hisVarsityFee = 4250;
+   char hisVarsityGreat = 'A';
+
+   // Values
+   printf("Character Value : %c \n", Name);
+   printf("integer Value : %d \n", hisRoll);
+   printf("integer Value too : %d \n", hisAge);
+   printf("Floating Number : %f \n", hisVarsityFee);
+   printf("Character Number : %c \n", hisVarsityGreat);
+   return 0;
+} 
